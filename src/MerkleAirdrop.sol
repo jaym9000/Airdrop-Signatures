0902f3bc7c83fa7a0182602a02629a5dcf06487a
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2024-12-28 22:36:14 -0400

    Completed base of Airdrop contract

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,7 +1,55 @@
 // SPDX-Licence-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 contract MerkleAirdrop {
-// some list of addresses
-// allow someone in the list to claim ERC-20 tokens
+    using SafeERC20 for IERC20;
+    // some list of addresses
+    // allow someone in the list to claim ERC-20 tokens
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error MerkleAirdrop__InvalidProof();
+
+    /*//////////////////////////////////////////////////////////////
+                               VARIABLES
+    //////////////////////////////////////////////////////////////*/
+
+    address[] claimers;
+    bytes32 private immutable i_merkleroot;
+    IERC20 private immutable i_airdropToken;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event Claim(address account, uint256 amount);
+
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        // some code
+        i_merkleroot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                FUNCTION
+    //////////////////////////////////////////////////////////////*/
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // calculate using the account and the amount, the hash -> leaf node
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleroot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
 }
